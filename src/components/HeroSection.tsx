03d9929119e5a1e02d283d855ede04ab9d0db085
techc0der <sandipsuthar086@gmail.com>
2025-11-01 14:20:20 +0530

    fixed error

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from 'react';
+import  { useRef, useEffect, useState } from 'react';
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
